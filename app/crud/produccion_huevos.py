0f88a723b8783aa75e657e3facc953a6df897fc7
m4r1no0 <168233712+m4r1no0@users.noreply.github.com>
2025-11-13 11:11:25 -0500

    Refactor SQL query to include joins and specific fields

--- app/crud/produccion_huevos.py
+++ app/crud/produccion_huevos.py
@@ -41,7 +41,17 @@ def get_produccion_huevos_by_id(db: Session, produccion_id: int):
 def get_all_produccion_huevos(db: Session):
     try:
         query = text("""
-            SELECT * FROM produccion_huevos;
+            SELECT 
+                produccion_huevos.id_produccion,
+                galpones.nombre AS nombre_galpon,
+                produccion_huevos.cantidad,
+                produccion_huevos.fecha,
+                tipo_huevos.tamaño
+            FROM produccion_huevos
+            LEFT JOIN tipo_huevos 
+                ON produccion_huevos.id_tipo_huevo = tipo_huevos.id_tipo_huevo
+            LEFT JOIN galpones 
+                ON produccion_huevos.id_galpon = galpones.id_galpon;
 
         """)
         result = db.execute(query).mappings().all()
